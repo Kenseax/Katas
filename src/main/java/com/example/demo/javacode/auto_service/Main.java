754263556248ae515cf1dfa3cd06378f88cf0566
Kenseax <70656830+Kenseax@users.noreply.github.com>
2022-10-11 01:08:31 +0300

    Автосервис/Техосмотр

--- src/main/java/com/example/demo/javacode/auto_service/Main.java
+++ src/main/java/com/example/demo/javacode/auto_service/Main.java
@@ -12,14 +12,19 @@ public class Main {
     private static Bus bus = new Bus("04.04.05", 10943, 200_000 );
 
     public static void main(String[] args) throws AutoServiceFuckedUpException {
+
         AutoService<Car> carService = new AutoService<>();
+        //Выводит Exception
         carService.checkTypeOfAutoAndPrintResult(car);
 
         AutoService<Truck> truckService = new AutoService<>();
+        //Выводит Exception
         truckService.checkTypeOfAutoAndPrintResult(truck);
+        //Отработает нормально
         truckService.checkTypeOfAutoAndPrintResult(truck2);
 
         AutoService<Bus> busAutoService = new AutoService<>();
+        //Выведет ответ для автобуса
         busAutoService.checkTypeOfAutoAndPrintResult(bus);
     }
 
