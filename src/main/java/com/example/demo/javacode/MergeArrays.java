36b93372a94d1b28da90127f88bde0b2383ff3ea
Vadim <kv3@list.ru>
2023-04-18 12:55:23 +0300

    added MergeArrays

--- src/main/java/com/example/demo/javacode/MergeArrays.java
+++ src/main/java/com/example/demo/javacode/MergeArrays.java
@@ -9,8 +9,8 @@ import java.util.List;
 public class MergeArrays {
 
     public static void main(String[] args) {
-        int[] arr1 = {1, 5, 3, 2, 5, 76, 7};
-        int[] arr2 = {4, 3, 7, 4, 1, 2, 3, 4, 6, 7};
+        int[] arr1 = {1, 5, 3, 7};
+        int[] arr2 = {4, 2, 6, 8};
 
         // для вывода массива
         System.out.println(Arrays.toString(concatenate(arr1, arr2)));
@@ -30,6 +30,8 @@ public class MergeArrays {
         return arr;
     }
 
+    // Методы для массивов объектов НЕ примитивов !!!
+
     static <T> T[] concatenate1(T[] array1, T[] array2) {
         List<T> resultList = new ArrayList<>(array1.length + array2.length);
         Collections.addAll(resultList, array1);
@@ -43,8 +45,7 @@ public class MergeArrays {
 
     // Общий метод для объединения массивов одного типа в Java
     public static <T> T[] concatenate2(T[] first, T[] second) {
-        T[] ob = (T[]) Array.newInstance(first.getClass().getComponentType(),
-                first.length + second.length);
+        T[] ob = (T[]) Array.newInstance(first.getClass().getComponentType(), first.length + second.length);
 
         System.arraycopy(first, 0, ob, 0, first.length);
         System.arraycopy(second, 0, ob, first.length, second.length);
